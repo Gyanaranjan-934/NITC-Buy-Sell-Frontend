7410514501b50eb2ebd9798f22d156ae5a654673
Gyana <gyanaranjansahoo509@gmail.com>
2023-10-10 19:33:34 +0530

    new initial commit for frontend

--- server/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const fetchuser = require('../middlewares/fetchUser');
-const { createChat, getChats } = require('../controllers/chatController');
-const router = express.Router();
-
-router.route('/').post(fetchuser,createChat);
-router.route('/').get(fetchuser,getChats);
-
-module.exports = router;
